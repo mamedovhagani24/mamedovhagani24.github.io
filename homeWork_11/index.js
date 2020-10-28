66c171c7bfac0c8e217b54e40b6b9780554c31e6
Максим Мамедов <frondenddeveloper@gmail.com>
2020-10-28 20:48:19 +0200

    homeWork_11

--- homeWork_11/index.js
+++ homeWork_11/index.js
@@ -1,21 +1,4 @@
-// task 10 -----------------------------------------
-
-// const someValue = {}
-
-
-// function isEmpty(obj) {
-//     for (let key in obj) {
-//         return false
-//     }
-
-//     return true
-// }
-
-// console.log(isEmpty(someValue))
-
-// task 11 ---------------------------------------------
-
-let obj = {
+const obj = {
     "0": "zero",
     "42": "answer",
     "greeting": "Hello",
@@ -39,26 +22,3 @@ function returnNUmberKey(obj) {
 
 returnNUmberKey(obj)
 
-// let salaries = {
-//     HRs: { Alice: 500, Bob: 700 },
-//     Devs: { Carol: 2500, Carlos: 1000, Charles: 1500 },
-//     QAs: { Chuck: 1650, Craig: 850, Dave: 3000 }
-
-// };
-
-// userPractic
-
-// let obj = {
-
-// }
-
-// function addObj(o, k, v) {
-//     return o[k] = v
-// }
-
-// addObj(obj, 'name', 'Xaqani')
-// addObj(obj, 'surname', 'Mamedov')
-// addObj(obj, 'age', 20)
-// addObj(obj, 'nickName', 'Mike ')
-
-// console.log(obj)
