5e9bcdc1070a589ff2c60bebd14e89d08120d4b5
Максим Мамедов <frondenddeveloper@gmail.com>
2020-10-31 13:21:33 +0200

    HomeWork

--- homeWork_12/index.js
+++ homeWork_12/index.js
@@ -1,19 +1,18 @@
 const salaries = {
     HRs: { Alice: 500, Bob: 700 },
     Devs: { Carol: 2500, Carlos: 1000, Charles: 1500 },
-    QAs: { Chuck: 1650, Craig: 850, Dave: 3000 }
+    QAs: { Chuck: 1650, Craig: 850, Dave: 3000 },
 };
 
-let newSalaries = {}
+let newSalaries = {};
 
 for (let key in salaries) {
     for (let value in salaries[key]) {
-        newSalaries[key] = {...salaries[key]}
-        newSalaries[key][value] *= 2
-
+        newSalaries[key] = { ...salaries[key] };
+        newSalaries[key][value] *= 2;
     }
 }
 
-console.log(newSalaries)
+console.log(newSalaries);
 
-console.log(salaries)
+console.log(salaries);
