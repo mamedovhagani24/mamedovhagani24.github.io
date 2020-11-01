eda6bc18bccfb5488e483e1285a0d64a2d627614
Максим Мамедов <frondenddeveloper@gmail.com>
2020-11-01 19:11:39 +0200

    homeWork

---
+++ homeWork_9/index.js
@@ -0,0 +1,56 @@
+function gameMoreLess(genericNumber) {
+
+    alert('Добро пожаловать на игру')
+
+    inProccess(genericNumber)
+}
+
+function returnRandomNumber () {
+    return Math.floor((Math.random() * 10) + 1)
+}
+
+
+function inProccess(genericNumber) {
+    let isProccess = true
+
+    while(isProccess) {
+        const userNumber = +prompt('Введите число')
+
+        if (!userNumber) {
+            return noValue();
+        }
+
+        if (userNumber > genericNumber) {
+               alert("Много!");
+          } else if (userNumber < genericNumber) {
+               alert("Мало!");
+          } else if (userNumber < 1 || userNumber > 10) {
+               alert("Не тот диапазон!");
+          } else if(isNaN(userNumber)) {
+               alert ('Это не число')
+          } else {
+               isProccess = false
+               return success()
+          }
+    }
+}
+
+function success() {
+    alert ('Вы угадали')
+
+    const playAgain = confirm('ты хочешь поиграть снова?')
+
+    if (playAgain) return gameMoreLess(returnRandomNumber())
+
+    alert( 'GameOver' )
+}
+
+function noValue () {
+    alert('Игра окончена')
+
+    const playAgain = confirm('ты хочешь поиграть снова?')
+    if (playAgain) return gameMoreLess(returnRandomNumber())
+    alert( 'GameOver' )
+}
+
+gameMoreLess(returnRandomNumber())
