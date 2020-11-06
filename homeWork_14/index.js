1023d44d7098d3caec55792ee28648d35ab20019
Максим Мамедов <frondenddeveloper@gmail.com>
2020-11-06 22:47:50 +0200

    HomeWork

---
+++ homeWork_14/index.js
@@ -0,0 +1,25 @@
+// Создайте массив [‘Apple’, ‘Banana’, ‘Pineapple’]
+
+let fruits = ['Apple', 'Banana', 'Pineapple']
+console.log(fruits)
+
+// Добавьте в конец массива ‘Orange’ и ‘Grapes’
+
+fruits.push('Orange', 'Grapes')
+console.log(fruits)
+
+// Замените значение ‘Banana’ на ‘Peer’
+
+fruits[1] = 'Peer'
+console.log(fruits)
+
+// Удалите последний элемент массива и выведите его в консоль
+
+const popFruit = fruits.pop()
+console.log(popFruit)
+
+
+// Добавьте в начало массива “Watermelon”
+
+fruits.unshift('Watermelon')
+console.log(fruits)
