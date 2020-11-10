3ec3ce110b11fe2b9c4648669f8b79289e287ffc
Максим Мамедов <frondenddeveloper@gmail.com>
2020-11-10 20:39:45 +0200

    homeWork

---
+++ homeWork_16/index.js
@@ -0,0 +1,37 @@
+'use script'
+
+const number = {
+    key: undefined
+}
+
+console.log('key' in number)
+
+// const arr = [
+//   {name: 'Vasya', surname: 'Ivanov'},
+//   {name: 'Vanya', surname: 'Ivanov'},
+//   {name: 'Albert', surname: 'Vasyliev'},
+// ]
+
+// function keyBy(arr, key) {
+  
+//   return arr.reduce((userByKey, arr) => {
+
+//     let objKey = arr[key]
+
+//     if(!userByKey[objKey]) {
+//       userByKey[objKey] = []
+//     }
+
+//     userByKey[objKey].push(arr)
+
+//     return userByKey
+
+//   }, {})
+// }
+
+// console.log(keyBy(arr, 'surname'))
+// console.log(keyBy(arr, 'name'))
+
+
+
+
