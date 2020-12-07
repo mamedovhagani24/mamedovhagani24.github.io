5a47d945b80a569cca963de444f18f63af517bc2
Максим Мамедов <frondenddeveloper@gmail.com>
2020-12-07 16:50:29 +0200

    homeWork

--- homeWork_24/index.js
+++ homeWork_24/index.js
@@ -2,31 +2,31 @@
 
 function createRangeFilter(min, max) {
 
-    return function(elem) {
-        return elem >= min && elem <= max ? true : false;
+    return function (elem) {
+        return elem >= min && elem <= max
     }
 }
 
-let array = [2, 2, 3, 66, 5, 1]
+let array = [2, 2, 2, 3, 66, 5, 1]
 let newArr = array.filter(createRangeFilter(2, 4))
 console.log(newArr)
 
 // N:2
 
 const arr = [
-  { name: "Vasya", surname: "Ivanov" },
-  { name: "Vanya", surname: "Ivanov" },
-  { name: "Albert", surname: "Vasyliev" },
+    { name: "Vasya", surname: "Ivanov" },
+    { name: "Vanya", surname: "Ivanov" },
+    { name: "Albert", surname: "Vasyliev" },
 ];
 
 function createKeyBy(propName) {
 
-    return function(arr) {
+    return function (arr) {
         return arr.reduce((userByKey, arr) => {
             let objKey = arr[propName];
 
             if (!userByKey[objKey]) {
-            userByKey[objKey] = [];
+                userByKey[objKey] = [];
             }
 
             userByKey[objKey].push(arr);
