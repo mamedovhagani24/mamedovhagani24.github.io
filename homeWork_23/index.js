10f0f08f516af7bff47886595294836fd0d1dc03
Максим Мамедов <frondenddeveloper@gmail.com>
2020-12-07 17:29:11 +0200

    homeWork

--- homeWork_23/index.js
+++ homeWork_23/index.js
@@ -1,32 +1,33 @@
-let obj = {
-    name: 'Xaqani',
-    surname: 'Mamedov',
-    age: 20,
-    about: {
-        city: 'Kharkiv',
-        isAdmin: [
-            true, 20
-        ]
-    }
-}
+// let obj = {
+//     name: 'Xaqani',
+//     surname: 'Mamedov',
+//     age: 20,
+//     about: {
+//         city: 'Kharkiv',
+//         isAdmin: [
+//             true, 20
+//         ]
+//     }
+// }
+
+// let obj = [1, 2, 3, 4, 5]
 
 
+// function cloneObj(obj) {
+//     const newObj = Array.isArray(obj) ? [] : {}
+//     for (let key in obj) {
+//         if (typeof obj[key] == 'object') {
+//             newObj[key] = cloneObj(obj[key])
+//         } else {
+//             newObj[key] = obj[key]
+//         }
+//     }
+//     return newObj
+// }
 
-function cloneObj(obj) {
-    const newObj = {}
-    for (let key in obj) {
-        if (typeof obj[key] == 'object') {
-            newObj[key] = cloneObj(obj[key])
-        } else {
-            newObj[key] = obj[key]
-        }
-    }
-    return newObj
-}
 
+// console.log(cloneObj(obj))
 
-console.log(cloneObj(obj))
+// // obj.about.isAdmin.isTrue = false
 
-obj.about.isAdmin.isTrue = false
-
-console.log(obj)
+// console.log(obj)
