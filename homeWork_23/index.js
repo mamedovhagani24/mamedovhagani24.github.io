559892092a62f505e86942824034149c4a4cd33e
Максим Мамедов <frondenddeveloper@gmail.com>
2020-12-07 17:25:45 +0200

    homeWork

--- homeWork_23/index.js
+++ homeWork_23/index.js
@@ -3,22 +3,30 @@ let obj = {
     surname: 'Mamedov',
     age: 20,
     about: {
-        city: 'Kharkiv'
+        city: 'Kharkiv',
+        isAdmin: [
+            true, 20
+        ]
     }
 }
 
 
-function cloneObj(obj, newObj = {}) {
-    for(let key in obj) {
-        if(typeof obj[key] == 'object') {
-            newObj[key] = cloneObj(obj[key], Array.isArray(obj[key]) ? [] : {})
+
+function cloneObj(obj) {
+    const newObj = {}
+    for (let key in obj) {
+        if (typeof obj[key] == 'object') {
+            newObj[key] = cloneObj(obj[key])
         } else {
             newObj[key] = obj[key]
         }
     }
-
     return newObj
 }
 
 
 console.log(cloneObj(obj))
+
+obj.about.isAdmin.isTrue = false
+
+console.log(obj)
